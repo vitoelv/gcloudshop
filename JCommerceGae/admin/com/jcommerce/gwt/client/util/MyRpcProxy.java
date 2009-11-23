996697689aa15ceff4d5936f56daa1954ee72ea0
zouljs@gmail.com <zouljs@gmail.com@94e08764-61fd-11de-b259-67f869b4f23a>
2009-11-23 03:36:55 +0000

    make the field "criteria" public so the class PagingListService can access it and set the criteria with the arg "criteria".Correct the reference to criteria.

--- JCommerceGae/admin/com/jcommerce/gwt/client/util/MyRpcProxy.java
+++ JCommerceGae/admin/com/jcommerce/gwt/client/util/MyRpcProxy.java
@@ -1,10 +1,13 @@
 package com.jcommerce.gwt.client.util;
 
+import java.util.List;
+
 import com.extjs.gxt.ui.client.data.RpcProxy;
 import com.jcommerce.gwt.client.service.Criteria;
 
 public abstract class MyRpcProxy<D> extends RpcProxy<D> {
-	Criteria criteria = null;
+	public Criteria criteria = null;
+
 	public void setCriteria(Criteria criteria) {
 		this.criteria = criteria;
 	}
