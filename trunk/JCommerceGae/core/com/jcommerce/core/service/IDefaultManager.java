41c3e519cb4ef51bd345c65223f94c85de6396a4
yongli21cn <yongli21cn@94e08764-61fd-11de-b259-67f869b4f23a>
2009-11-26 04:10:42 +0000

    added:
    two new APIs in DefaultManagerImpl:
    deleteAll() and getListByIds() (use with caution)

--- trunk/JCommerceGae/core/com/jcommerce/core/service/IDefaultManager.java
+++ trunk/JCommerceGae/core/com/jcommerce/core/service/IDefaultManager.java
@@ -4,8 +4,8 @@
 
 package com.jcommerce.core.service;
 
+import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 import com.jcommerce.core.model.ModelObject;
 
@@ -16,6 +16,22 @@ public interface IDefaultManager {
 	
 	public int getCount(String modelName, Criteria criteria);
 	public int getList (List res, String modelName, Criteria criteria, final int firstRow, final int maxRow);
+	
+	/**
+	 * Return a list of objects based on the list of ids
+	 * However if object with a given id does not exist, will just neglect it rather than throw exception
+	 * which implies the the size of the returned list could be less than the size of the input list
+	 * 
+	 * DataStore do not provide "In" or "Or". So this is just a multiple-call to dataStore and in-memory merge
+	 * Use it in caution when the size of ids is large
+	 * 
+	 * @param modelName
+	 * @param ids
+	 * @return
+	 */
+	public List getListByIds (String modelName, List<String> ids);
+	
+	
 	public List getList (String modelName, Criteria criteria);
 	public List getList (String modelName, Criteria criteria, final int firstRow, final int maxRow);
 	public void getListOfChild(List res, String modelName, String fieldName, String parentId);
@@ -24,6 +40,7 @@ public interface IDefaultManager {
 	public String txattach (ModelObject to);
 	public boolean txupdate (ModelObject to);
 	public boolean txdelete (String modelName, String id);
+	public void txdeleteall (Collection<ModelObject> objs);
 	
 	
 
