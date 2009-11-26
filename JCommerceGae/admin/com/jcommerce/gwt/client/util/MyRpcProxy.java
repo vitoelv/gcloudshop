9d2ea3e7003eb8ffa6bdbc89c84714f5e20107f5
yongli21cn <yongli21cn@94e08764-61fd-11de-b259-67f869b4f23a>
2009-11-26 04:11:49 +0000

    hide the criteria variable

--- JCommerceGae/admin/com/jcommerce/gwt/client/util/MyRpcProxy.java
+++ JCommerceGae/admin/com/jcommerce/gwt/client/util/MyRpcProxy.java
@@ -1,12 +1,10 @@
 package com.jcommerce.gwt.client.util;
 
-import java.util.List;
-
 import com.extjs.gxt.ui.client.data.RpcProxy;
 import com.jcommerce.gwt.client.service.Criteria;
 
 public abstract class MyRpcProxy<D> extends RpcProxy<D> {
-	public Criteria criteria = null;
+	private Criteria criteria = null;
 
 	public void setCriteria(Criteria criteria) {
 		this.criteria = criteria;
