766f7a8ad974b45a5319940d0b0f53a87f2b0a93
wu_weijun@vanceinfo.com <wu_weijun@vanceinfo.com@94e08764-61fd-11de-b259-67f869b4f23a>
2009-12-06 08:49:29 +0000

    complete the comment list in user center 1. CommentAction.java  get the email of user from json when the user don't login 2. UserAction.java  add to action commnet_list to get comment list and del_cmt to delete a comment 3. CommnetWrapper.java  add method called by ftl to get data 4. LibMain.java add method to get comment list 5. ShopConfigWrapper.java  add variable to define the number of comment per page

--- JCommerceGae/web/com/jcommerce/web/to/CommentWrapper.java
+++ JCommerceGae/web/com/jcommerce/web/to/CommentWrapper.java
@@ -1,11 +1,21 @@
 package com.jcommerce.web.to;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+
 import com.jcommerce.core.model.Comment;
+import com.jcommerce.core.model.Goods;
 import com.jcommerce.core.model.ModelObject;
+import com.jcommerce.core.service.Condition;
+import com.jcommerce.core.service.Criteria;
+import com.jcommerce.core.service.IDefaultManager;
+import com.jcommerce.gwt.client.ModelNames;
+import com.jcommerce.gwt.client.model.IComment;
 
 public class CommentWrapper extends BaseModelWrapper {
 
 	Comment comment;
+	private IDefaultManager manager = null;
 	@Override
 	protected Object getWrapped() {
 		return getComment();
@@ -18,5 +28,46 @@ public class CommentWrapper extends BaseModelWrapper {
 	public Comment getComment() {
 		return comment;
 	}
+	
+	public String getCommentType() {
+		return getComment().getCommentType().toString();
+	}
+	
+	public String getCmtName() {
+		String goodsId = getComment().getIdValue();
+		Goods goods = (Goods) manager.get(ModelNames.GOODS, goodsId);
+		return goods.getGoodsName();
+	}
 
+	public String getFormatedAddTime() {
+		Long addTime = getComment().getAddTime();
+		SimpleDateFormat formatter = new SimpleDateFormat ("yyyy-MM-dd HH:mm:ss");
+		return formatter.format(addTime);
+	}
+	
+	public String getCommentId() {
+		return getComment().getPkId();
+	}
+	
+	public String getContent() {
+		return getComment().getContent();
+	}
+	
+	public String getReplyContent() {
+		String id = getComment().getPkId();
+		Criteria criteria = new Criteria();
+		criteria.addCondition(new Condition(IComment.PARENT_ID, Condition.EQUALS, id));
+		List reply = manager.getList(ModelNames.COMMENT, criteria);
+		if(reply.size() == 0) {
+			return null;
+		}
+		else {
+			Comment comment = (Comment) reply.get(0);
+			return comment.getContent();
+		}
+	}
+	
+	public void setManager(IDefaultManager manager) {
+		this.manager = manager;
+	}
 }
