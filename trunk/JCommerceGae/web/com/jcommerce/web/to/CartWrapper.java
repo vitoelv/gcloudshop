85b5761240f56f21b6133ba7c662f9c4fb788f53
wu_weijun@vanceinfo.com <wu_weijun@vanceinfo.com@94e08764-61fd-11de-b259-67f869b4f23a>
2009-12-22 11:14:44 +0000

    show promote price in goods cart when good sales promotion

--- trunk/JCommerceGae/web/com/jcommerce/web/to/CartWrapper.java
+++ trunk/JCommerceGae/web/com/jcommerce/web/to/CartWrapper.java
@@ -1,16 +1,25 @@
 package com.jcommerce.web.to;
 
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Set;
 
 import com.jcommerce.core.model.Cart;
+import com.jcommerce.core.model.Goods;
 import com.jcommerce.core.model.GoodsAttr;
 import com.jcommerce.core.model.ModelObject;
+import com.jcommerce.core.service.IDefaultManager;
+import com.jcommerce.gwt.client.ModelNames;
 import com.jcommerce.web.util.WebFormatUtils;
 
 public class CartWrapper extends BaseModelWrapper {
 
 	Cart cart;
+	private IDefaultManager manager;
+	
+	public void setManager(IDefaultManager manager) {
+		this.manager = manager;
+	}
 	@Override
 	protected Object getWrapped() {
 		return getCart();
@@ -34,8 +43,37 @@ public class CartWrapper extends BaseModelWrapper {
 	}
 	
 	public String getFormatedGoodsPrice() {
-		return WebFormatUtils.priceFormat(getCart().getGoodsPrice());
+		String goodsId = getCart().getGoodsId();
+		Goods goods = (Goods) manager.get(ModelNames.GOODS, goodsId);
+		
+		//判断是否促销
+		Long promoteEndTime = goods.getPromoteEndDate();
+		Long promoteStartTime = goods.getPromoteStartDate();
+		Long nowTime = new Date().getTime();
+		if(nowTime > promoteEndTime || nowTime < promoteStartTime) {
+			return WebFormatUtils.priceFormat(getCart().getGoodsPrice());
+		}
+		else {
+			return WebFormatUtils.priceFormat(goods.getPromotePrice());
+		}		
 	}
+	
+	public double getPrice() {
+		String goodsId = getCart().getGoodsId();
+		Goods goods = (Goods) manager.get(ModelNames.GOODS, goodsId);
+		
+		//判断是否促销
+		Long promoteEndTime = goods.getPromoteEndDate();
+		Long promoteStartTime = goods.getPromoteStartDate();
+		Long nowTime = new Date().getTime();
+		if(nowTime > promoteEndTime || nowTime < promoteStartTime) {
+			return getCart().getGoodsPrice();
+		}
+		else {
+			return goods.getPromotePrice();
+		}
+	}
+	
 	public String getFormatedSubtotal() {
 		return WebFormatUtils.priceFormat(getCart().getGoodsPrice() * getCart().getGoodsNumber());
 	}
