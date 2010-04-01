c46867f039bc2c4d17cb5f4bd66497c8577e5bd2
yongli21cn <yongli21cn@94e08764-61fd-11de-b259-67f869b4f23a>
2010-04-01 02:10:29 +0000

    upgrade to GAE Java1.3.2

--- JCommerceGae/release/test-release-site.bat
+++ JCommerceGae/release/test-release-site.bat
@@ -1,6 +1,6 @@
 
 rem change this
 set JAVA_HOME=D:\Program Files\Java\jdk1.6.0_17
-set GAE_HOME=D:\JCommerce\appengine-java-sdk-1.3.1
+set GAE_HOME=D:\JCommerce\appengine-java-sdk-1.3.2
 
-call %GAE_HOME%\bin\dev_appserver.cmd war-site\war
+call %GAE_HOME%\bin\dev_appserver.cmd -p 8030 war-site\war
