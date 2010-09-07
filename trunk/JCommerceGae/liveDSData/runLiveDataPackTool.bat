45163f0176d66b08e85ac42d64ccf3286e17f6ae
yongli21cn <yongli21cn@94e08764-61fd-11de-b259-67f869b4f23a>
2010-09-07 07:07:46 +0000

    1. Updated data packing tool, 2. added English version of demo data

--- trunk/JCommerceGae/liveDSData/runLiveDataPackTool.bat
+++ trunk/JCommerceGae/liveDSData/runLiveDataPackTool.bat
@@ -4,10 +4,10 @@ echo pack live data
 echo usage:
 echo create a folder (e.g. "bigger"), then copy your customized files (mydata.txt and files folder) under liveDSData/bigger, then run this tool. 
 
-set rootPath=D:/JCommerce/JCommerceGae/liveDSData/bigger
-set outFileName=data.zip
+set rootPath=D:/JCommerce/JCommerceGae/liveDSData/demo_en
+set outFileName=demo_en.zip
 
-echo generating %outFileName% from folder %rootPath% ... 
+echo generating %rootPath%/%outFileName% ... 
 
 java LiveDataPackTool %rootPath% %outFileName% 
 
