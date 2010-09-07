efe44875ccd8056a62886113724c3c3b7f60d03d
yongli21cn <yongli21cn@94e08764-61fd-11de-b259-67f869b4f23a>
2010-09-07 07:09:07 +0000

    upgrade to SDK1.3.7

--- JCommerceGae/install.cmd
+++ JCommerceGae/install.cmd
@@ -1,7 +1,7 @@
 echo off
 rem change this
 set JAVA_HOME=D:\Program Files\Java\jdk1.6.0_17
-set GAE_HOME=D:\JCommerce\appengine-java-sdk-1.3.5
+set GAE_HOME=D:\JCommerce\appengine-java-sdk-1.3.7
 
 rem DO NOT change
 Set CLASSPATH=%GAE_HOME%/lib/appengine-tools-api.jar
